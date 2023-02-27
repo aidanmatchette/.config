f7f630fa9ce5e0c8cc17afec88e1b298af9e595e
Aidan Matchette <aidan.j.matchette@gmail.com>
2023-02-27 10:43:48 -0800

    Changes colorscheme and added bash script

--- nvim/lua/aidan/plugins/kanagawa.lua
+++ nvim/lua/aidan/plugins/kanagawa.lua
@@ -5,14 +5,6 @@ if (not status) then
 end
 
 kanagawa.setup({
-    commentStyle = { italic = true },
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    variablebuiltinStyle = { italic = true},
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
     transparent = true,        -- do not set background color
-    globalStatus = false,       -- adjust window separators highlight for laststatus=3
-    terminalColors = true,      -- define vim.g.terminal_color_{0,17}
     theme = "default"           -- Load "default" theme or the experimental "light" theme
 })
